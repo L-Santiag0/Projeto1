33b196f8d898d89107955390ad3099aea0b3c09d
Lucas Santiago <lucascarmo2811@gmail.com>
2023-08-27 17:04:58 -0300

    Create meuprog1.cpp
    Resolvendo os exercícios (1 ao 3) da "Instrução Prática 1": "meuprog1.cpp".

---
+++ meuprog1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    
+    string nome_do_usuario;
+    cout << "Digite seu nome: ";
+    cin >> nome_do_usuario;
+    cout << "Bom dia, " << nome_do_usuario << "!" << endl;
+    
+    return 0;
+}
